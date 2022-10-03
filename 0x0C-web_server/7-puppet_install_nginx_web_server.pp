d82ad92dd5f5ba886f3ae12e0a2b866f8eb13570
amba05 <emma.amba@gmail.com>
2022-10-03 09:38:33 +0000

    WEB-SERVER

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec { 'nginx server':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://github.com/belovetech;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://github.com/amba05;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
